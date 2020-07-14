60c911050b2093f44323664ca1951b7c21036b71
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-14 18:09:59 -0400

    change app name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'nile-api'
+const mongooseBaseName = 'aiolo-api'
 
 // create the mongodb uri for development and test
 const database = {
