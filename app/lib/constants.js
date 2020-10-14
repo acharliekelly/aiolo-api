845d0e6dc5cdbf71642f65e26da388431c226ee9
Charlie Kelly <charliekelly31415@gmail.com>
2020-10-14 00:31:41 -0400

    constants

---
+++ app/lib/constants.js
@@ -0,0 +1,18 @@
+
+export const ORIENTATION = {
+  PORTRAIT: -1,
+  LANDSCAPE: 1,
+  SQUARE: 0
+};
+
+export const FILTER_KEYS = {
+  SUBJECT: 'subject',
+  LOCATION: 'location',
+  STYLE: 'style',
+  SEASON: 'season',
+  COLOR: 'color',
+  PERIOD: 'period',
+  MEDIUM: 'medium',
+  ALBUM: 'album',
+  AVAILABILITY: 'availability'
+};
